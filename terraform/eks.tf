99cdb9ef234f5f5df0023e6dc0491692eb6d581c
Khessie <khessieevans@yahoo.com>
2023-03-19 17:17:10 +0000

    removed eks security group from eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -46,36 +46,36 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 }
 
 
-# EKS Cluster Security Group
-resource "aws_security_group" "eks_cluster" {
-  name        = "${var.env_prefix}-cluster-sg"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.my-web_vpc.id
+# # EKS Cluster Security Group
+# resource "aws_security_group" "eks_cluster" {
+#   name        = "${var.env_prefix}-cluster-sg"
+#   description = "Cluster communication with worker nodes"
+#   vpc_id      = aws_vpc.my-web_vpc.id
 
-  tags = {
-    Name = "${var.env_prefix}-cluster-sg"
-  }
-}
+#   tags = {
+#     Name = "${var.env_prefix}-cluster-sg"
+#   }
+# }
 
-resource "aws_security_group_rule" "cluster_inbound" {
-  description              = "Allow worker nodes to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 443
-  type                     = "ingress"
-}
+# resource "aws_security_group_rule" "cluster_inbound" {
+#   description              = "Allow worker nodes to communicate with the cluster API Server"
+#   from_port                = 443
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.eks_cluster.id
+#   source_security_group_id = aws_security_group.eks_nodes.id
+#   to_port                  = 443
+#   type                     = "ingress"
+# }
 
-resource "aws_security_group_rule" "cluster_outbound" {
-  description              = "Allow cluster API Server to communicate with the worker nodes"
-  from_port                = 1024
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 65535
-  type                     = "egress"
-}
+# resource "aws_security_group_rule" "cluster_outbound" {
+#   description              = "Allow cluster API Server to communicate with the worker nodes"
+#   from_port                = 1024
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.eks_cluster.id
+#   source_security_group_id = aws_security_group.eks_nodes.id
+#   to_port                  = 65535
+#   type                     = "egress"
+# }
 
 # EKS Node Groups
 resource "aws_eks_node_group" "k8s_ng" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "vpc_cidr_block" {}
-variable "private_subnet_cidr_blocks" {}
+variable "subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
 variable "env_prefix" {}
 variable "availability_zone" {}
+variable "subnet_names" {}
