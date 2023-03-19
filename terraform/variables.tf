6af12195eb23de81f8431f89b798661e76824a5e
Khessie <khessieevans@yahoo.com>
2023-03-19 17:23:36 +0000

    another fucking edit. Tf!!!!

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,4 @@
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_blocks" {}
-variable "public_subnet_cidr_blocks" {}
 variable "env_prefix" {}
 variable "availability_zone" {}
 variable "subnet_names" {}
