69e6d88fef79847b85f4a225f5fd74c9a1681c4f
Khessie <khessieevans@yahoo.com>
2023-03-20 18:59:22 +0000

    edited

--- nginx-controller/helm.nginx.tf
+++ nginx-controller/helm.nginx.tf
@@ -20,7 +20,7 @@ resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "4.5.2"
+  version    = "3.36.0"
 
   namespace        = kubernetes_namespace.nginx-namespace.id
   create_namespace = true
