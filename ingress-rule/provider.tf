e968f409486f12f6901bb3a019102e3f3206b0b2
Khessie <khessieevans@yahoo.com>
2023-03-20 18:43:45 +0000

    edited again

--- ingress-rule/provider.tf
+++ ingress-rule/provider.tf
@@ -7,20 +7,20 @@ terraform {
   }
 }
 
-data "aws_eks_cluster" "k8s" {
-  name = "k8s"
+data "aws_eks_cluster" "demo" {
+  name = "demo"
 }
-data "aws_eks_cluster_auth" "k8s_auth" {
-  name = "k8s_auth"
+data "aws_eks_cluster_auth" "demo_auth" {
+  name = "demo_auth"
 }
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.k8s.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.k8s.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.demo.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
   version                = "2.16.1"
   config_path            = "~/.kube/config"
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", "k8s"]
+    api_version = "client.authentication.demo.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", "demo"]
     command     = "aws"
   }
 }
